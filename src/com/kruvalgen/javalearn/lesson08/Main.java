51d823ace0b4314a2f001bda90328660cfcd0b85
kruvalgen <kruvalgen@yandex.ru>
2021-09-13 22:34:12 +0300

    Init

--- src/com/kruvalgen/javalearn/lesson08/Main.java
+++ src/com/kruvalgen/javalearn/lesson08/Main.java
@@ -2,10 +2,10 @@ package com.kruvalgen.javalearn.lesson08;
 
 public class Main {
     public static void main(String[] args) {
-//        String str = "Text";
-//        for (int i = 0; i <= 10; i++) {
-//            System.out.println(str);
-//        }
+        String str = "Text";
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(str);
+        }
 
 
     }
